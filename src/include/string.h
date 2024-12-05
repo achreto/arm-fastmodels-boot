c2a828bfb6b7f3c9f205c7887a9cc5b095d7d6e6
Reto Achermann <achreto@gmail.com>
2024-12-05 23:34:22 +0000

    adding bzero function
    Signed-off-by: Reto Achermann <achreto@gmail.com>

---
+++ src/include/string.h
@@ -0,0 +1,4 @@
+
+#include <types.h>
+void bzero(void *, size_t n);
+
--- src/string.c
+++ src/string.c
@@ -7,3 +7,12 @@
  * Copyright 2020 Reto Achermann
  * SPDX-License-Identifier: GPL-3.0
  */
+
+#include <string.h>
+
+void bzero(void *buf, size_t n)
+{
+    for (size_t i = 0; i < n; i++) {
+        ((char *)buf)[i] = 0;
+    }
+}
